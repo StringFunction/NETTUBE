44fb1b4a5e9ff8ca41c30de6e7d9388b85e1e9d7
cleciocarmais <franciscoclecioti@gmail.com>
2024-06-09 21:11:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var convertBtn = document.querySelector('.convert-button'); 
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => { 
+    console.log(`URL: ${URLinput.value}`); 
+    sendURL(URLinput.value); 
+});
+async function  sendURL( URL ) { 
+    fetch(`http://localhost:4000/download?URL=${URL}`, { 
+        método:'GET' 
+    }).then( res => res.json()) 
+    .then( json => console.log(json)); 
+    window.location.href = `http://localhost:3000/download?URL=${URL}`; 
+
+
+}
