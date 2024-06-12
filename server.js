8fe149e7bcfb54d146cd2a359e989084e90d6e2a
cleciocarmais <franciscoclecioti@gmail.com>
2024-06-12 15:16:07 -0300

    finish

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/",(req, res) =>{
-  res.sendFile(__dirname + "public/index.html")
+  res.sendFile(__dirname + "/public/index.html")
 
 } )
 app.get('/download', ( req , res ) => { 
