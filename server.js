6f19371adde7366d37cd28a70051a1baaf392553
cleciocarmais <franciscoclecioti@gmail.com>
2024-06-12 14:50:40 -0300

    teste 20

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'))
 
 
 app.get("/",(req, res) =>{
-  res.sendFile(__dirname + "index.html")
+  res.sendFile(__dirname + "./index.html")
 } )
 app.get('/download', ( req , res ) => { 
 
